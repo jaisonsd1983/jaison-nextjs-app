e91f22639975746d2d13d83d3b7f8de7fbf08776
Jaison <jaisonjose1983@gmail.com>
2024-10-04 12:12:10 +0530

    The NEXT JS app foundation/primary installation/boilerplate

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="mb-4 text-4xl">This next js app is in progress...</h1>
+    </div>
+  );
+}
